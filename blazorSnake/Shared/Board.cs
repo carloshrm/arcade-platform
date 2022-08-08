df718a4d9c371d60bf0ebf344ebf865df50ff725
Carlos Moraes <carloshrmoraes@gmail.com>
2022-08-08 14:03:50 -0300

    Add project files.

---
+++ blazorSnake/Shared/Board.cs
@@ -0,0 +1,141 @@
+﻿using Blazor.Extensions.Canvas.Canvas2D;
+
+namespace blazorSnake.Shared
+{
+    public class Board
+    {
+        private Canvas2DContext canvasContext { get; set; }
+        private (int r, int c) scaleOfset { get; set; }
+        private (int row, int col) limits { get; set; }
+        private (int r, int c) foodPosition { get; set; }
+        public Snake snake { get; set; }
+
+        public Board(int rows, int cols)
+        {
+            scaleOfset = (10, 10);
+            limits = (rows / scaleOfset.r, cols / scaleOfset.c);
+        }
+
+        public void setContext(Canvas2DContext c)
+        {
+            canvasContext = c;
+        }
+
+        public void makeFood()
+        {
+            Random rng = new Random();
+            int row, col;
+            bool invalid;
+            do
+            {
+                row = rng.Next(1, limits.row - 2);
+                col = rng.Next(1, limits.col - 2);
+
+                foreach (var pc in snake.tail)
+                {
+                    invalid = pc.pos.c == col || pc.pos.r == row;
+                }
+                invalid = snake.headPosition.c == col || snake.headPosition.r == row;
+            } while (invalid);
+            foodPosition = (row, col);
+        }
+
+        public void placeSnakeOnStart()
+        {
+            int i = snake.size;
+            while (i-- > 0)
+            {
+                snake.tail.Add(new Snake.TailPiece((
+                    snake.headPosition.r - (snake.movingDirection.row * i),
+                    snake.headPosition.c - (snake.movingDirection.col * i)),
+                    snake.size - i));
+            }
+        }
+
+        public bool moveSnake()
+        {
+            if (snake.headPosition.r >= limits.row || snake.headPosition.c >= limits.col)
+            {
+                return false;
+            }
+            else
+            {
+                foreach (var pc in snake.tail)
+                {
+                    if (pc.pos == snake.headPosition)
+                    {
+                        return false;
+                    }
+                }
+                if (snake.headPosition == foodPosition)
+                {
+                    snake.feedSnake();
+                    makeFood();
+                    return true;
+                }
+            }
+            snake.tail.Add(new Snake.TailPiece(snake.headPosition, snake.size));
+            return true;
+        }
+
+        private async Task drawPiece(int r, int c)
+        {
+            await canvasContext.FillRectAsync(r, c, scaleOfset.c, scaleOfset.r);
+        }
+
+        public async Task drawGameState()
+        {
+            await canvasContext.BeginBatchAsync();
+
+            await canvasContext.SetFillStyleAsync("red");
+            await drawPiece(foodPosition.c * scaleOfset.c, foodPosition.r * scaleOfset.r);
+
+            for (int i = 0; i < snake.tail.Count(); i++)
+            {
+                if (snake.tail[i].val <= 0)
+                {
+                    await canvasContext.SetFillStyleAsync("white");
+                    await drawPiece(snake.tail[i].pos.c * scaleOfset.c, snake.tail[i].pos.r * scaleOfset.r);
+                    snake.tail.Remove(snake.tail[i]);
+                }
+            }
+
+            await canvasContext.SetFillStyleAsync("darkgreen");
+            await drawPiece(snake.headPosition.c * scaleOfset.c, snake.headPosition.r * scaleOfset.r);
+
+            for (int i = 0; i < snake.tail.Count(); i++)
+            {
+                Console.WriteLine("parsing tail");
+                await drawPiece(snake.tail[i].pos.c * scaleOfset.c, snake.tail[i].pos.r * scaleOfset.r);
+                Console.WriteLine(snake.tail[i].val);
+                snake.tail[i].val--;
+            }
+            await canvasContext.EndBatchAsync();
+        }
+
+        public async void drawBoardEdges()
+        {
+            await canvasContext.BeginBatchAsync();
+            await canvasContext.SetFillStyleAsync("black");
+            for (int i = 0; i <= (limits.row * scaleOfset.r); i++)
+            {
+                await canvasContext.FillRectAsync(i, 0, scaleOfset.c, scaleOfset.r);
+                await drawPiece(i, 0);
+                await drawPiece(i, (limits.col * scaleOfset.c) - scaleOfset.c);
+            }
+            for (int j = 0; j <= (limits.col * scaleOfset.c); j++)
+            {
+                await drawPiece(0, j);
+                await drawPiece((limits.row * scaleOfset.r) - scaleOfset.r, j);
+            }
+            await canvasContext.EndBatchAsync();
+        }
+
+        public void newPlayer(Snake p)
+        {
+            snake = p;
+            snake.setStartingValues(limits.row, limits.col);
+        }
+
+    }
+}
---
+++ blazorSnake/Shared/Snake.cs
@@ -0,0 +1,68 @@
+﻿namespace blazorSnake.Shared
+{
+    public class Snake
+    {
+        public class TailPiece
+        {
+            public (int r, int c) pos { get; set; }
+            public int val { get; set; }
+            public TailPiece((int r, int c) pos, int val)
+            {
+                this.pos = pos;
+                this.val = val;
+            }
+        }
+
+        public int size { get; set; }
+        public (int r, int c) headPosition { get; set; }
+        public List<TailPiece> tail { get; set; }
+        public (int row, int col) movingDirection { get; set; }
+
+        public Snake(int startingSize)
+        {
+            size = startingSize;
+            tail = new List<TailPiece>();
+        }
+
+        public void setStartingValues(int r, int c)
+        {
+            headPosition = (r / 2, c / 2);
+            movingDirection = (0, 1);
+        }
+
+        public void feedSnake()
+        {
+            size++;
+        }
+
+        public void setNextSnakePosition()
+        {
+            headPosition = (headPosition.r + movingDirection.row, headPosition.c + movingDirection.col);
+        }
+
+        public void parseMoveCommand(string pressedKey)
+        {
+            switch (pressedKey)
+            {
+                case "ArrowUp":
+                    if (movingDirection != (1, 0))
+                        movingDirection = (-1, 0);
+                    break;
+                case "ArrowDown":
+                    if (movingDirection != (-1, 0))
+                        movingDirection = (1, 0);
+                    break;
+                case "ArrowLeft":
+                    if (movingDirection != (0, 1))
+                        movingDirection = (0, -1);
+                    break;
+                case "ArrowRight":
+                    if (movingDirection != (0, -1))
+                        movingDirection = (0, 1);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
