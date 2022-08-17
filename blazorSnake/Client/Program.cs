84066efdae4f1a8708957bbffc929be81a94e897
Carlos Moraes <carloshrmoraes@gmail.com>
2022-08-17 11:31:39 -0300

    adjusting layout

--- blazorSnake/Client/Program.cs
+++ blazorSnake/Client/Program.cs
@@ -8,9 +8,7 @@ namespace blazorSnake.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
             await builder.Build().RunAsync();
         }
     }
