8e29e1cf9d8bc6a2fc47dbbf0114810449ca77d1
Carlos Moraes <carloshrmoraes@gmail.com>
2022-10-05 16:37:30 -0300

    breaker ball and bounce logic implemented

--- gamesPlatform/Shared/Breaker/BreakerBall.cs
+++ gamesPlatform/Shared/Breaker/BreakerBall.cs
@@ -7,30 +7,37 @@
         public override int healthPoints { get; set; }
         public override GameAsset model { get; set; }
         public override int spriteSelect { get; set; }
-        private (int row, int col) movementOffset;
+        private (int row, int col) movementVector;
 
         public BreakerBall(int row, int col)
         {
+            model = BallModel.breakerBall;
             this.row = row;
             this.col = col;
             healthPoints = 0;
             spriteSelect = 0;
-            movementOffset = (3, 0);
-            model = BallModel.breakerBall;
+            movementVector = (4, 0);
         }
 
-        public void bounce()
+        public void bounce(int rDir, int cDir)
         {
-            movementOffset = (-3, 0);
+            movementVector = (movementVector.row * rDir, movementVector.col * cDir);
+            if (Math.Abs(movementVector.col) >= 6)
+                movementVector.col = (int)(movementVector.col * 0.8);
+        }
+
+        public void offsetVector(int accel)
+        {
+            movementVector.col -= accel / 10;
+            Console.WriteLine(movementVector);
         }
 
         public override bool updatePosition((int row, int col) limits)
         {
-            if (row <= limits.row && row >= 0 &&
-                col <= limits.col && col >= 0)
+            if (row <= limits.row)
             {
-                row += movementOffset.row;
-                col += movementOffset.col;
+                row += movementVector.row;
+                col += movementVector.col;
                 return true;
             }
             else
--- gamesPlatform/Shared/Breaker/BreakerGame.cs
+++ gamesPlatform/Shared/Breaker/BreakerGame.cs
@@ -1,18 +1,18 @@
 ﻿namespace cmArcade.Shared.Breaker
 {
-    public class BreakerField
+    public class BreakerGame
     {
         public BreakerPad player { get; set; }
         //list<blocks>
         public List<BreakerBall> balls { get; set; }
         public (int row, int col) limits { get; set; }
 
-        public BreakerField((int row, int col) limits)
+        public BreakerGame((int row, int col) limits)
         {
             this.player = new BreakerPad(limits.row - (limits.row / 14), limits.col / 2);
             this.balls = new List<BreakerBall>();
             this.limits = limits;
-            setBall(limits.row / 2, player.col);
+            setBall(limits.row / 2, player.col + (player.model.width / 2) + 10);
         }
 
         private void setBall(int row, int col)
@@ -24,7 +24,7 @@
         {
             player.updatePosition(limits);
             checkCollision();
-            balls.RemoveAll(b => b.updatePosition(limits) == false);
+            balls.RemoveAll(b => !b.updatePosition(limits));
             if (balls.Count == 0) setBall(limits.row / 2, player.col);
         }
 
@@ -32,15 +32,32 @@
         {
             foreach (var b in balls)
             {
-                if ((b.row + b.model.height >= player.row) &&
-                    b.col + b.model.width >= player.col &&
-                    b.col <= player.col + player.model.width
-                    )
+                if (checkHit(b, player))
                 {
-                    b.bounce();
-                    Console.WriteLine("bounce at" + Math.Abs((player.model.width / 2) - (b.col + (b.model.width / 2) - player.col)));
+                    b.bounce(-1, -1);
+                    b.offsetVector(calcOffsetPercentage(b, player.col, player.model.width));
+                }
+                else if ((b.col <= 0) || (b.col >= limits.col))
+                {
+                    b.bounce(1, -1);
+                }
+                else if (b.row <= 0)
+                {
+                    b.bounce(-1, 1);
                 }
             }
+
+            int calcOffsetPercentage(GameObject obj, int edgePos, int edgeWidth)
+            {
+                double offset = edgePos + (edgeWidth / 2) - (obj.col + (obj.model.width / 2));
+                return (int)(offset * 100 / edgeWidth);
+            }
+        }
+
+        private bool checkHit(GameObject obj, GameObject target)
+        {
+            return (obj.row + obj.model.height >= target.row) && (obj.row <= target.row + target.model.height) &&
+                    (obj.col + obj.model.width >= target.col) && (obj.col <= target.col + target.model.width);
         }
 
         public void parseKeyDown(string input)
