fcd3c4de728e022f618cec34cb36f6fe38bbbee4
Carlos Moraes <carloshrmoraes@gmail.com>
2023-03-15 16:26:05 -0300

    changing game object abstract class into interface

---
+++ gamesPlatform/Shared/IGameObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+namespace cmArcade.Shared
+{
+    public interface IGameObject
+    {
+        List<GraphicAsset>? decals { get; set; }
+        int healthPoints { get; set; }
+        GraphicAsset model { get; set; }
+        Vector2 pos { get; set; }
+        int spriteSelect { get; set; }
+        bool updatePosition((int row, int col) limits);
+    }
+}
