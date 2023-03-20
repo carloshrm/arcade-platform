1b5fb10b3a9bcd1437cb554d2f7c1eaf5bfb9938
Carlos Moraes <carloshrmoraes@gmail.com>
2023-03-20 16:47:09 -0300

    abstract class for simple objects with no assets

---
+++ gamesPlatform/Shared/ISimpleGameObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace cmArcade.Shared
+{
+    public interface ISimpleGameObject
+    {
+        CanvasRenderedAsset model { get; set; }
+        Vector2 pos { get; set; }
+    }
+}
