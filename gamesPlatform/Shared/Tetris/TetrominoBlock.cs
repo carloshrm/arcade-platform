b324b82b32b1b4e71e6c3eeb177e774366c49705
Carlos Moraes <carloshrmoraes@gmail.com>
2023-04-02 15:33:28 -0300

    model adjustment details for spin logic

---
+++ gamesPlatform/Shared/Tetris/TetrominoBlock.cs
@@ -0,0 +1,11 @@
+﻿namespace cmArcade.Shared.Tetris
+{
+    public class TetrominoBlock : CanvasRenderedAsset
+    {
+        public override string color { get; init; }
+        public override float width { get; init; }
+        public override float height { get; init; }
+
+        public static readonly TetrominoBlock simpleBlock = new TetrominoBlock { color = "white", width = 1, height = 1 };
+    }
+}
---
+++ gamesPlatform/Shared/Tetris/TetrominoPart.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace cmArcade.Shared.Tetris
+{
+    public class TetrominoPart : ISimpleGameObject
+    {
+        public CanvasRenderedAsset model { get; set; }
+        public Vector2 pos { get; set; }
+        public bool isPivot { get; set; }
+
+        public TetrominoPart(Vector2 pos)
+        {
+            this.pos = pos;
+            model = TetrominoBlock.simpleBlock;
+            isPivot = false;
+        }
+
+        public override string? ToString()
+        {
+            return "pos: " + pos + " || pvt? " + isPivot;
+        }
+    }
+}
