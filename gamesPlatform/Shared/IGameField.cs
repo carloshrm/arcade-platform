a17ddeb0bfa8c3bbbd24f08ba4ff5677805d8c1e
Carlos Moraes <carloshrmoraes@gmail.com>
2023-04-10 11:49:51 -0300

    changing interface to support score updated from field

--- gamesPlatform/Shared/IGameField.cs
+++ gamesPlatform/Shared/IGameField.cs
@@ -3,7 +3,7 @@
     public interface IGameField
     {
         public Object getPlayer();
-        public void updateGameState();
+        public void updateGameState(Score s);
         public bool checkGameOver();
         public void setMessage(string msg);
         public void setScoreMultiplier(int val);
