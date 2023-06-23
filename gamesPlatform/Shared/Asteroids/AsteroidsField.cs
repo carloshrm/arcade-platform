c0f53e01e75e96a1282bffacc6255099c09c8ace
Carlos Moraes <carloshrmoraes@gmail.com>
2023-06-23 10:39:39 -0300

    asteroids base pages

---
+++ gamesPlatform/Shared/Asteroids/AsteroidsField.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmArcade.Shared.Asteroids;
+
+public class AsteroidsField : IGameField
+{
+    public bool checkGameOver()
+    {
+        throw new NotImplementedException();
+    }
+
+    public object getPlayer()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void setMessage(string msg)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void setScoreMultiplier(int val)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void updateGameState(Score s)
+    {
+        throw new NotImplementedException();
+    }
+}
