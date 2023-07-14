19605a1e7cd6b2cb95fe874f3d64ac447d90e2a1
Carlos Moraes <carloshrmoraes@gmail.com>
2023-07-14 10:05:23 -0300

    implementing shot logic

---
+++ gamesPlatform/Shared/Asteroids/Shot.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace cmArcade.Shared.Asteroids;
+
+public class Shot
+{
+    public Vector2 pos { get; set; }
+    public Vector2 dir { get; set; }
+
+    public bool fade { get; set; } = false;
+    private System.Timers.Timer lifetime = new System.Timers.Timer() { Enabled = true, Interval = 2000 };
+
+    public Shot()
+    {
+        lifetime.Elapsed += (o, e) => fade = true;
+    }
+
+    public void UpdatePosition()
+    {
+        pos += Vector2.Multiply(dir, 2);
+    }
+}
