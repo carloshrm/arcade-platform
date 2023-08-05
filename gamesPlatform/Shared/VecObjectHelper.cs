dd8e282c3fbc0b64b089c56028915f09fdfb504e
Carlos Moraes <carloshrmoraes@gmail.com>
2023-08-05 10:39:11 -0300

    implementing extension methods for distance

--- gamesPlatform/Shared/VecObjectHelper.cs
+++ gamesPlatform/Shared/VecObjectHelper.cs
@@ -10,11 +10,21 @@ public static class VecObjectHelper
         foreach (var cPt in obj.model.points)
         {
             if (closestPoint == Vector2.Zero ||
-                Vector2.Distance(cPt + obj.pos, targetPos) <= Vector2.Distance(closestPoint, targetPos))
+                closestPoint.CompareDistanceTo(targetPos))
             {
                 closestPoint = cPt + obj.pos;
             }
         }
         return closestPoint;
+    }
+
+    public static bool CompareDistanceTo(this Vector2 obj, Vector2 target)
+    {
+        return (obj.X * obj.X) + (obj.Y * obj.Y) > (target.X * target.X) + (target.Y * target.Y);
+    }
+
+    public static float GetManhattanDistanceTo(this Vector2 obj, Vector2 target) 
+    { 
+        return Math.Abs(obj.X - target.X) + Math.Abs(obj.Y - target.Y);
     }
 }
