b8d4130511003c9c26ce88d4316c786a05824a60
Carlos Moraes <carloshrmoraes@gmail.com>
2023-08-07 16:41:48 -0300

    fixed NaN crash on normalizing vector.

--- gamesPlatform/Shared/Asteroids/Asteroid.cs
+++ gamesPlatform/Shared/Asteroids/Asteroid.cs
@@ -1,4 +1,7 @@
-﻿using System.Numerics;
+﻿using System.Diagnostics;
+using System.Numerics;
+
+using Timer = System.Timers.Timer;
 
 namespace cmArcade.Shared.Asteroids;
 
@@ -10,7 +13,9 @@ public class Asteroid : ISimpleVectorialObject
     public bool wasHit { get; set; } = false;
     public bool isPrimary { get; set; }
 
-    public Vector2 floatDir { get; private set; }    
+    private float bumpLimit { get; set; }
+
+    public Vector2 floatDir { get; set; }    
 
     public Asteroid(Vector2 pos, bool isPrimary = true)
     {
@@ -27,11 +32,27 @@ public class Asteroid : ISimpleVectorialObject
         else if (pos.Y < 0) pos = new Vector2(pos.X, yEdge);
         else if (pos.X >= xEdge) pos = new Vector2(0, pos.Y);
         else if (pos.Y >= yEdge) pos = new Vector2(pos.X, 0);
+
+        if (bumpLimit > 0)
+        {
+            Console.WriteLine(bumpLimit);
+            bumpLimit--;
+        }
+        if (floatDir.X == float.NaN || pos.X == float.NaN)
+            Debugger.Break();
     }
 
-    public void SetFloatDir(Vector2 dir)
+    public void SetNormalizedFloatDir(Vector2 dir)
     {
-        floatDir = Vector2.Normalize(dir);
+        double normX = Math.Round(dir.X / dir.Length(), 2);
+        double normY = Math.Round(dir.Y / dir.Length(), 2); 
+
+        if (double.IsNaN(normX))
+            normX = Random.Shared.Next(0, 1);
+        if (double.IsNaN(normY))
+            normY = Random.Shared.Next(0, 1);
+
+        floatDir = new Vector2((float)normX, (float)normY);
     }
 
     public override bool Equals(object? obj)
@@ -39,7 +60,18 @@ public class Asteroid : ISimpleVectorialObject
         if (obj == null || obj is not Asteroid)
             return false;
         else
-            return (int)pos.X == (int)((Asteroid)obj).pos.X 
+            return (int)pos.X == (int)((Asteroid)obj).pos.X
                 && (int)pos.Y == (int)((Asteroid)obj).pos.Y;
+    }
+
+    public void Bump(Vector2 dir)
+    {
+        if (bumpLimit < 10)
+        {
+            var negated = Vector2.Negate(floatDir);
+
+            SetNormalizedFloatDir(negated * dir);
+            bumpLimit += 50;
+        }
     }
 }
