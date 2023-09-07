12959a92ad281c1cc4e72856263d0d39586f5d17
Carlos Moraes <carloshrmoraes@gmail.com>
2023-09-07 14:33:52 -0300

    base JS scripts for testing net7 interop

---
+++ gamesPlatform/Client/wwwroot/GameCanvas.js
@@ -0,0 +1,22 @@
+﻿function getWindowWidth() {
+    return window.innerWidth;
+}
+
+function getWindowHeight() {
+    return window.innerHeight;
+}
+
+function setFocus() {
+    document.getElementById("game-container").focus();
+}
+
+function isMobile() {
+    return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent);
+}
+
+export {
+    getWindowWidth,
+    getWindowHeight,
+    setFocus,
+    isMobile
+}
