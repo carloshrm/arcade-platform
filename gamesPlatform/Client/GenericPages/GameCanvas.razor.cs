90f5f07e31948c924302e6c340c977a4d88b9f0a
Carlos Moraes <carloshrmoraes@gmail.com>
2023-09-11 15:58:29 -0300

    add partial class for mapping interop

---
+++ gamesPlatform/Client/GenericPages/GameCanvas.razor.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
+
+namespace cmArcade.Client.GenericPages;
+
+[SupportedOSPlatform("browser")]
+public partial class GameCanvas
+{
+    //[JSImport("Test", "GameCanvas")]
+    //internal static partial string DoTest(string msg);
+
+    [JSImport("getWindowWidth", "GameCanvas")]
+    internal static partial float GetWindowWidth();
+
+    [JSImport("getWindowHeight", "GameCanvas")]
+    internal static partial float GetWindowHeight();
+
+    [JSImport("setFocus", "GameCanvas")]
+    internal static partial void SetWindowFocus();
+
+    [JSImport("isMobile", "GameCanvas")]
+    internal static partial bool IsBrowserMobile();
+}
