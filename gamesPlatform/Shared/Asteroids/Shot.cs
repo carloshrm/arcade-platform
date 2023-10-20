cbe2c30203c97488c7db0814c32e38ea1d670cfa
Carlos Moraes <carloshrmoraes@gmail.com>
2023-10-20 15:55:08 -0300

    increase laser speed

--- gamesPlatform/Shared/Asteroids/Shot.cs
+++ gamesPlatform/Shared/Asteroids/Shot.cs
@@ -5,16 +5,13 @@ public class Shot : ISimpleVectorialObject
 {
     public Vector2 pos { get; set; }
     public Vector2 dir { get; set; }
-
     public bool fade { get; set; } = false;
-
     public CanvasRenderedVectorial model { get; set; }
-
 
     public Shot(Vector2 pos, Vector2 dir)
     {
         this.pos = pos;
-        this.dir = dir * 2.5f;
+        this.dir = dir * 5f;
         Task.Delay(1400).ContinueWith(_ => fade = true);
         model = new ShotModel(new List<Vector2>() { dir, this.dir });
     }
