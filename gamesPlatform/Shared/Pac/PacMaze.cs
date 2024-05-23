24ea9e23704b7f08a790e06a986b260e6bb4c637
Carlos Moraes <carloshrmoraes@gmail.com>
2024-05-23 10:36:12 -0300

    change maze to vectorial

--- gamesPlatform/Shared/Pac/PacMaze.cs
+++ gamesPlatform/Shared/Pac/PacMaze.cs
@@ -2,40 +2,52 @@
 
 namespace cmArcade.Shared.Pac;
 
-public class PacMaze : IGameObject
+public class PacMaze : ISimpleVectorialObject
 {
     public bool[][] collisionMap { get; set; }
-    public GraphicAsset model { get; set; }
     public List<(int x, int y)> powerUps { get; set; }
-
-    public List<GraphicAsset>? decals { get; set; }
-    public int healthPoints { get; set; } = -1;
     public Vector2 pos { get; set; } = Vector2.Zero;
-    public int spriteSelect { get; set; } = 1;
+    public CanvasRenderedVectorial model { get; set; }
 
     public PacMaze()
     {
-        model = PacMazeModel.testMaze;
+        model = PacMazeModel.GetTestMaze();
         //var test = File.ReadLines("./test.txt");
         collisionMap = (new bool[1280][]).Select(l => l = new bool[720]).ToArray();
-    }
-
-    public bool UpdatePosition((float row, float col) limits)
-    {
-        throw new NotImplementedException();
+        foreach (var pt in model.points)
+        {
+            for (int i = pt; i < length; i++)
+            {
+                
+            }
+        }
     }
 }
 
-public class PacMazeModel : GraphicAsset
+public class PacMazeModel(IEnumerable<Vector2> points) : CanvasRenderedVectorial(points)
 {
-    public override string spriteId { get; set; }
-    public override int width { get; init; }
-    public override int height { get; init; }
+    public override string strokeColor { get; set; }
+    public override float strokeLineWidth { get; set; }
+    public override string? fillColor { get; set; }
+    public override IEnumerable<Vector2> points { get; set; }
+    public override float objWidth { get; set; }
+    public override float objHeight { get; set; }
+    public override Vector2 topRightBounds { get; set; }
+    public override Vector2 bottomLeftBounds { get; set; }
 
-    public static readonly PacMazeModel testMaze = new()
+    public static PacMazeModel GetTestMaze() => 
+        new(new List<Vector2> 
+        { 
+            new Vector2(200, 700),
+            new Vector2(600, 700),
+            new Vector2(600, 500),
+            new Vector2(800, 500),
+            new Vector2(800, 700),
+            new Vector2(1000, 700),
+        })
     {
-        spriteId = "test-maze",
-        width = 1280,
-        height = 720
+        strokeColor = "#00FF00",
+        strokeLineWidth = 3,
+        fillColor = "#AF00F0",
     };
 }
