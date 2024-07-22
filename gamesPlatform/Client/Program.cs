0cf37c671928704d40d4d476a83e552a5e1cab06
Carlos Moraes <carloshrmoraes@gmail.com>
2024-07-22 15:23:56 -0300

    add test token

--- gamesPlatform/Client/Program.cs
+++ gamesPlatform/Client/Program.cs
@@ -9,6 +9,7 @@ public class Program
     public static async Task Main(string[] args)
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        // test eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwibmFtZSI6IlRlc3QgdXNlciIsImlhdCI6MTUxNjIzOTAyMn0.Sn_z5C6EbAWQz5nb1zbxil6X4IDTc19JVxAAQZvUI4Y
         builder.RootComponents.Add<App>("#app");
         builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
         if (OperatingSystem.IsBrowser())
